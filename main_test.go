f88689ff7f799b90eb97708003f6e80bf683ec41
Вязовский Дмитрий Николаевич <Pepidastr@yandex.com>
2025-02-24 19:07:07 +0500

    done

--- main_test.go
+++ main_test.go
@@ -1,49 +1,85 @@
 package main
 
 import (
+	"database/sql"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	_ "modernc.org/sqlite"
 )
 
 func Test_SelectClient_WhenOk(t *testing.T) {
-	// настройте подключение к БД
 
+	db, err := sql.Open("sqlite", "demo.db")
+	require.NoError(t, err)
 	clientID := 1
+	client, err := selectClient(db, clientID)
+	require.NoError(t, err)
 
-	// напиши тест здесь
+	assert.Equal(t, client.ID, clientID)
+	assert.NotEmpty(t, map[string]string{
+		"FIO":      client.FIO,
+		"Login":    client.Login,
+		"Birthday": client.Birthday,
+		"Email":    client.Email,
+	})
+
 }
 
 func Test_SelectClient_WhenNoClient(t *testing.T) {
-	// настройте подключение к БД
 
+	db, err := sql.Open("sqlite", "demo.db")
+	require.NoError(t, err)
 	clientID := -1
+	client, err := selectClient(db, clientID)
 
-	// напиши тест здесь
+	require.Equal(t, err, sql.ErrNoRows)
+
+	assert.Empty(t, client)
+
 }
 
 func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
-	// настройте подключение к БД
-
+	db, err := sql.Open("sqlite", "demo.db")
+	require.NoError(t, err)
 	cl := Client{
 		FIO:      "Test",
 		Login:    "Test",
 		Birthday: "19700101",
 		Email:    "mail@mail.com",
 	}
+	cl.ID, err = insertClient(db, cl)
+	require.NoError(t, err)
+	require.NotEmpty(t, cl.ID)
 
-	// напиши тест здесь
+	clSelect, err := selectClient(db, cl.ID)
+	require.NoError(t, err)
+
+	assert.Equal(t, cl, clSelect)
 }
 
 func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
-	// настройте подключение к БД
 
+	db, err := sql.Open("sqlite", "demo.db")
+	require.NoError(t, err)
 	cl := Client{
 		FIO:      "Test",
 		Login:    "Test",
 		Birthday: "19700101",
 		Email:    "mail@mail.com",
 	}
+	id, err := insertClient(db, cl)
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
 
-	// напиши тест здесь
+	_, err = selectClient(db, id)
+	require.NoError(t, err)
+
+	err = deleteClient(db, id)
+	require.NoError(t, err)
+
+	_, err = selectClient(db, id)
+	require.Equal(t, err, sql.ErrNoRows)
+
 }
